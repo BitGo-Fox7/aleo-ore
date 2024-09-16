ea53ef555515c0f5402bab87f7c0f35d58c709c6
Fox7 <taiyingkj@gmail.com>
2024-09-17 04:09:48 +0800

    Update aleo-ore.sh

--- aleo-ore.sh
+++ aleo-ore.sh
@@ -18,21 +18,21 @@ echo "Cloning ore-mine-pool repository..."
 git clone https://github.com/xintai6660707/ore-mine-pool.git /home/ore-mine-pool
 
 # 下载 aleo_prover-v0.1.1_hot.tar.gz 到 /home 目录
-echo "Downloading aleo_prover-v0.1.1_hot.tar.gz to /home..."
-wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/v0.1.1-hot/aleo_prover-v0.1.1_hot.tar.gz -P /home
+echo "Downloading aleo_prover-v0.1.0_hot.tar.gz to /home..."
+wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/v0.1.1-hot/aleo_prover-v0.1.0_hot.tar.gz -P /home
 
-# 解压 aleo_prover-v0.1.1_hot.tar.gz 到 /home
+# 解压 aleo_prover-v0.1.0_hot.tar.gz 到 /home
 echo "Extracting aleo_prover..."
-tar -xzvf /home/aleo_prover-v0.1.1_hot.tar.gz -C /home
+tar -xzvf /home/aleo_prover-v0.1.0_hot.tar.gz -C /home
 
 # 复制 ore-mine-pool-linux 和 aleo_prover 到 aleo-ore 目录
 echo "Copying ore-mine-pool-linux and aleo_prover to aleo-ore..."
 cp /home/ore-mine-pool/ore-mine-pool-linux "$BASE_DIR"
 cp /home/aleo_prover/aleo_prover "$BASE_DIR"
 
-# 删除 /home 目录下的 ore-mine-pool 文件夹和 aleo_prover-v0.1.1_hot.tar.gz 压缩包
+# 删除 /home 目录下的 ore-mine-pool 文件夹和 aleo_prover-v0.1.0_hot.tar.gz 压缩包
 echo "Removing unnecessary files and directories in /home..."
-rm -rf /home/ore-mine-pool /home/aleo_prover /home/aleo_prover-v0.1.1_hot.tar.gz
+rm -rf /home/ore-mine-pool /home/aleo_prover /home/aleo_prover-v0.1.0_hot.tar.gz
 
 # 赋予 aleo-ore 文件夹中所有文件执行权限
 echo "Setting executable permissions for all files in aleo-ore..."
